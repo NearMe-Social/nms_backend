cfca0525ef12059b5f0b9f5291ac145d54136603
tolarom <tolarom27@gmail.com>
2026-04-30 07:24:13 +0700

    feat: configure TypeORM with AppDataSource and import Comments and Posts modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CommentsModule } from './modules/comments/comments.module';
+import { PostsModule } from './modules/posts/posts.module';
+import { AppDataSource } from './data-source';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(AppDataSource.options),
+    CommentsModule,
+    PostsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
