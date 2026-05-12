26ce65d769b013d4fb4629f7b3714cacf1e7acea
Sophat_Odom <freedomearning123@gmail.com>
2026-05-12 16:24:43 +0700

    change origin from using localhost to using the vercel domain url one in main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe, ClassSerializerInterceptor } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: 'https://nmsfrontend.vercel.app/',
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe({
