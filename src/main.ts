113fafa9024ce1a4c794fb5976ca238e1327b7f0
Sophat_Odom <freedomearning123@gmail.com>
2026-05-13 11:02:00 +0700

    deleting / from the enableCors which make it different from the actual frontend url

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe, ClassSerializerInterceptor } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://nmsfrontend.vercel.app/',
+    origin: 'https://nmsfrontend.vercel.app',
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe({
