2b485cd113bffccff5798e3aaacffa1f174eab7f
SokhomPanha <sokhompanha70@gmail.com>
2026-05-14 22:14:14 +0700

    Block user endpoint

--- src/modules/blocks/blocks.controller.ts
+++ src/modules/blocks/blocks.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { CreateBlockDto } from './dto/create-block.dto';
+import { UserBlock } from './entities/user-block.entity';
+import { BlocksService } from './blocks.service';
 
 @Controller('blocks')
-export class BlocksController {}
+export class BlocksController {
+
+      constructor(private readonly blocksService: BlocksService) {}
+    
+    @Post()
+    @HttpCode(HttpStatus.CREATED)
+    block(@Body() createBlockDto: CreateBlockDto): Promise<UserBlock> {
+        return this.blocksService.block(createBlockDto);
+  }
+
+}
--- src/modules/blocks/blocks.service.ts
+++ src/modules/blocks/blocks.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, ConflictException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { UserBlock } from './entities/user-block.entity';
+import { CreateBlockDto } from './dto/create-block.dto';
 
 @Injectable()
 export class BlocksService {
@@ -9,6 +10,27 @@ export class BlocksService {
     @InjectRepository(UserBlock)
     private readonly blockRepo: Repository<UserBlock>,
   ) {}
+
+    async block(createBlockDto: CreateBlockDto): Promise<UserBlock> {
+    const { blocker_id, blocked_user_id } = createBlockDto;
+ 
+    if (blocker_id === blocked_user_id) {
+      throw new BadRequestException('You cannot block yourself');
+    }
+ 
+    const existing = await this.isBlocked(blocker_id, blocked_user_id);
+    if (existing) {
+      throw new ConflictException('User is already blocked');
+    }
+ 
+    const block = this.blockRepo.create({
+      blocker: { user_id: blocker_id },
+      blockedUser: { user_id: blocked_user_id },
+    });
+ 
+    return this.blockRepo.save(block);
+  }
+
 
   async isBlocked(blockerId: number, blockedUserId: number): Promise<boolean> {
     const block = await this.blockRepo.findOne({
