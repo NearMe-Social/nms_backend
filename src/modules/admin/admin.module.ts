d21d48897c5ab10bd640573873a72bca1fac3000
Sophat_Odom <freedomearning123@gmail.com>
2026-05-16 15:09:15 +0700

    adding Report entity to the adminModule so that the service can use it

--- src/modules/admin/admin.module.ts
+++ src/modules/admin/admin.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AdminController } from './adminReports.controller';
 import { AdminService } from './adminReports.service';
+import { Report } from '../reports/entities/report.entities';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forFeature([Report])],
   providers: [AdminService],
   controllers: [AdminController],
 })
