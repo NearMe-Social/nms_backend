7c7827eed4df389399ab6cf983d1f5e14ba1849b
Sophat_Odom <freedomearning123@gmail.com>
2026-06-10 11:42:36 +0700

    fixing google login

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -6,10 +6,14 @@ import { LoginDto } from './dto/login.dto';
 import { SendOtpDto, VerifyOtpDto } from './dto/verify-otp.dto';
 import { JwtAuthGuard } from './guards/jwt-auth.guard';
 import {GoogleAuthGuard} from './guards/google.auth.guard';
+import { ConfigService } from '@nestjs/config';
+import { GoogleUser } from './strategies/google.strategy';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly configService: ConfigService,) {}
 
   @Post('register')
   register(@Body() registerDto: RegisterDto) {
@@ -34,16 +38,15 @@ export class AuthController {
   @UseGuards(GoogleAuthGuard)
   @Get('google/callback')
 async googleAuthCallback(
-  @Request() req: any,
+  @Request() req: { user: GoogleUser },
   @Res() res: any,
 ) {
   const result = await this.authService.googleLogin(
     req.user,
   );
+  const frontendUrl = this.configService.getOrThrow<string>('FRONTEND_URL');
 
-  return res.redirect(
-    `http://localhost:5173/auth/google/callback?token=${result.token}`,
-  );
+  return res.redirect(`${frontendUrl}/auth/google/callback?token=${encodeURIComponent(result.token)}`);
 }
 
   // ── NEW OTP endpoints ──
